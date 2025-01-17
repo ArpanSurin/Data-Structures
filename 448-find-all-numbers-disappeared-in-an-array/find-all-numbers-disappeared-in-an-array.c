12c1c93ee1710279171f5851dcedd8521950d9a2
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2025-01-17 22:11:31 +0530

    Time: 6 ms (20.05%) | Memory: 35.3 MB (63.96%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.c
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.c
@@ -12,9 +12,36 @@ int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
     }
 
     for(int i=0; i<numsSize; i++){
-        if(nums[i] > 0)
+        if(nums[i] > 0) {
             result[index++] = i+1;
+        }
     }
     *returnSize = index;
     return result;
 }
+
+// /**
+//  * Note: The returned array must be malloced, assume caller calls free().
+//  */
+// int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+//     int* result = (int*)malloc(numsSize * sizeof(int));
+//     int index = 0;
+
+//     // Mark visited numbers
+//     for (int i = 0; i < numsSize; i++) {
+//         int val = abs(nums[i]) - 1; // Convert value to index
+//         if (nums[val] > 0) {
+//             nums[val] = -nums[val];
+//         }
+//     }
+
+//     // Find missing numbers
+//     for (int i = 0; i < numsSize; i++) { // Iterate from 0 to numsSize - 1
+//         if (nums[i] > 0) {
+//             result[index++] = i + 1; // Add 1 because numbers are 1-based
+//         }
+//     }
+
+//     *returnSize = index;
+//     return result;
+// }
