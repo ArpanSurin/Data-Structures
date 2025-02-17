e0b43c54e5aeb9b7e687217f499f1c08a4d09f41
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2025-02-17 23:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (96.92%) - LeetSync

---
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.c
@@ -0,0 +1,12 @@
+int xorAllNums(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int x1 = 0, x2 = 0;
+    if (nums1Size % 2 != 0)
+        for (int i=0; i<nums2Size; i++){
+            x1 ^= nums2[i];
+        }
+    if (nums2Size % 2 != 0)
+        for(int i=0; i<nums1Size; i++){
+            x2 ^= nums1[i];
+        }
+    return x1 ^ x2;
+}
