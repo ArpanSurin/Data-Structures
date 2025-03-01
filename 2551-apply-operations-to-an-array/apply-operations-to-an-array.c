00f89c40f7a4aed0e49f337d4c313db659e15577
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2025-03-01 23:18:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (33.88%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.c
@@ -0,0 +1,24 @@
+int* applyOperations(int* nums, int numsSize, int* returnSize) {
+    int* newNums = (int*)calloc(numsSize, sizeof(int)); 
+    int count = 0;
+    int i;
+
+    for (i = 0; i < numsSize - 1; i++) {
+        if (nums[i] != 0) {
+            if (nums[i] == nums[i + 1]) {
+                newNums[count] = nums[i] * 2;
+                i++; 
+            } else {
+                newNums[count] = nums[i];
+            }
+            count++;
+        }
+    }
+
+    if (i < numsSize && nums[i] != 0) {
+        newNums[count] = nums[i];
+    }
+
+    *returnSize = numsSize;
+    return newNums;
+}
