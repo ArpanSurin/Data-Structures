d675eb7eede170aff002f27aeb63a8f8e414c756
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2025-04-16 10:58:52 +0530

    Time: 5 ms (51.35%), Space: 15 MB (21.33%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> map;
+        for(int i=0; i<nums.size(); i++){
+            int a = nums[i];
+            int more = target - a;
+            // auto it = find(map.begin(), map.end(), more);
+            if(map.find(more) != map.end()){
+                return {map[more], i};
+            }
+            map[a] = i;
+        }
+        return {-1, -1};
+    }
+};
