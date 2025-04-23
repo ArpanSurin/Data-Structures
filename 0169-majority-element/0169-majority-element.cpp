1131163999b49f5688d27bbeb8ef1ae93bb27c0a
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2025-04-23 20:53:28 +0530

    Time: 0 ms (100%), Space: 28.2 MB (58.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 0;
-        int candidate = 0;
-        for(int num : nums){
-            if (count == 0) { candidate = num; }
-            if (num == candidate) { count += 1; }
-            else count -= 1;
-        }
-        return candidate;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        return nums[n/2];
     }
 };
