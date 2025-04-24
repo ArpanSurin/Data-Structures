64108f53c695100920232cf3fc5c5f2b7e76f3e5
Arpan Surin <150426561+ArpanSurin@users.noreply.github.com>
2025-04-24 17:38:31 +0530

    Time: 0 ms (100%), Space: 8.4 MB (62.44%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int k = 0, j = 0, i = 0;
-        int m = word1.size();
-        int n = word2.size();
-        char* result = (char*)malloc((m + n +1) * sizeof(char));
-        while(i<m && j<n){
-            result[k++] = word1[i++];
-            result[k++] = word2[j++];
+        string result = "";
+        int i = 0;
+        while(i < word1.size() || i < word2.size()){
+            if(i < word1.size()){
+                result += word1[i];
+            }
+            if(i < word2.size()){
+                result += word2[i];
+            }
+            i++;
         }
-
-        while(i<m){
-            result[k++] = word1[i++];
-        }
-
-        while(j<n){
-            result[k++] = word2[j++];
-        }
-        result[k] = '\0';
         return result;
     }
 };
